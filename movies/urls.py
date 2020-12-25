d660973dd6f99317b1ff3a691b50df24d6e9c5cb
Srajan Gupta <srajan.oel@gmail.com>
2020-12-25 21:50:01 +0530

    Configured URLs

--- movies/urls.py
+++ movies/urls.py
@@ -4,7 +4,9 @@ app_name='movies'
 urlpatterns = [
     path('',views.HomeView.as_view(),name='Home'),
     path('add',views.AddView.as_view(),name='Add'),
-    path('addmovie',views.AddMovieView,name='AddMovie'),
+    path('addmovie',views.addmovieview,name='AddMovie'),
     path('view',views.MoviesView.as_view(),name='View'),
-    path('edit',views.EditView.as_view(),name='Edit')
+    path('edit',views.EditView.as_view(),name='Edit'),
+    path('edit/<int:pk>',views.EditMovieView.as_view(),name='EditMovie'),
+    path('update',views.updatemovieview,name='UpdateMovie')
 ]
