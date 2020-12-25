0053ae340ede3bd3cb0453412020e2051c635fad
Srajan Gupta <srajan.oel@gmail.com>
2020-12-25 21:50:20 +0530

    Configured Viewss

--- movies/views.py
+++ movies/views.py
@@ -21,14 +21,30 @@ class MoviesView(generic.ListView):
     
 class EditView(generic.ListView):
     template_name='movies/Edit.html'
+    context_object_name='movies_list'
     def get_queryset(self):
-        return
-    
-    
+        return Movie.objects.all()
 
-def AddMovieView(request):
+class EditMovieView(generic.DetailView):
+    model=Movie
+    template_name='movies/EditMovie.html'
+
+def addmovieview(request):
     movie_name=request.POST['movie_name']
     movie_release_date=request.POST['movie_release_date']
     movie_summary=request.POST['movie_summary']
+    m=Movie(movie_name=movie_name,movie_release_date=movie_release_date,movie_summary=movie_summary)
+    m.save()
     return HttpResponseRedirect(reverse('movies:Home'))
 
+def updatemovieview(request):
+    movie_id=request.POST['movie_id']
+    movie_name=request.POST['movie_name']
+    movie_release_date=request.POST['movie_release_date']
+    movie_summary=request.POST['movie_summary']
+    m=Movie.objects.get(pk=movie_id)
+    m.movie_name=movie_name
+    m.movie_release_date=movie_release_date
+    m.movie_summary=movie_summary
+    m.save()
+    return HttpResponseRedirect(reverse('movies:Home'))
