2ea02a30221ba78e1727378bb1b469f279f7659d
Srajan Gupta <srajan.oel@gmail.com>
2020-12-25 15:59:27 +0530

    Created Movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    movie_name=models.CharField(max_length=300)
+    movie_release_date=models.DateField()
+    movie_summary=models.CharField(max_length=10000)
+    def __str__(self):
+        return self.movie_name+', released on '+str(self.movie_release_date)
